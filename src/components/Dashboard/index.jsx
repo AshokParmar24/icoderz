dc942d56773e7582a7cf49466fccbb77360a373f
AshokParmar24 <ashokparmar72038@gmail.com>
2025-05-31 12:17:08 +0530

    chnage

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -10,33 +10,52 @@ import {
   MenuItem,
   Button,
   IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  CircularProgress,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getProductList, productCategory } from "../../services/api";
+import {
+  deleteProuctApi,
+  getProductList,
+  productCategory,
+} from "../../services/api";
 import { updateProductList } from "../../redux/actions/productAction";
 import { updateCategoty } from "../../redux/actions/categoryAction";
 import { useNavigate } from "react-router-dom";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const [sortOrder, setSortOrder] = useState("asc");
   const [selectedCategory, setSelectedCategory] = useState("");
-
+  const [open, setOpen] = useState(false);
+  const [productData, setProductData] = useState(null);
   const { productList } = useSelector((state) => state?.productReducer);
   const { categoryList } = useSelector((state) => state?.categoryReducer);
+  const [loading, setLoading] = useState(false);
+  const [sortTitle, setSortTiltle] = useState("asc");
+
   const dispatch = useDispatch();
 
   const fetchProductList = async () => {
+    setLoading(true);
+
     try {
       const response = await getProductList();
       if (response.status === 200) {
         dispatch(updateProductList(response?.data));
+        setLoading(false);
       }
     } catch (error) {
       console.log("error", error);
+      setLoading(false);
     }
   };
 
@@ -56,115 +75,202 @@ const Dashboard = () => {
 
   const sortedProducts = productList
     .filter((p) => !selectedCategory || p.category === selectedCategory)
-    .sort((a, b) =>
-      sortOrder === "asc" ? a.price - b.price : b.price - a.price
-    );
+    .sort((a, b) => {
+      if (sortTitle) {
+        const titleA = a.title.toLowerCase();
+        const titleB = b.title.toLowerCase();
+        if (titleA < titleB) return sortTitle === "asc" ? -1 : 1;
+        if (titleA > titleB) return sortTitle === "asc" ? 1 : -1;
+        return 0;
+      }
+      return sortOrder === "asc" ? a.price - b.price : b.price - a.price;
+    });
 
-  const handleDelete = (id) => {};
+  const handleDelete = (product) => {
+    setOpen(true);
+    setProductData(product);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setProductData(null);
+  };
+
+  const confirmDeleteProduct = async () => {
+    try {
+      const response = await deleteProuctApi(productData?.id);
+      if (response.status === 200) {
+        setOpen(false);
+        setProductData(null);
+        fetchProductList();
+        toast.success("delete successfully");
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   return (
-    <Box padding={6}>
-      <Typography variant="h3">Product List</Typography>
-      <Box mb={3} display="flex" gap={2}>
-        <Select
-          value={sortOrder}
-          onChange={(e) => setSortOrder(e.target.value)}
-          displayEmpty
+    <>
+      {loading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+          width="100%"
         >
-          <MenuItem value="asc">Price: Low to High</MenuItem>
-          <MenuItem value="desc">Price: High to Low</MenuItem>
-        </Select>
-
-        <Select
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-          displayEmpty
-        >
-          <MenuItem value="">All Categories</MenuItem>
-          {categoryList.map((cat) => (
-            <MenuItem key={cat} value={cat}>
-              {cat}
-            </MenuItem>
-          ))}
-        </Select>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate("/new-product")}
-        >
-          Add Product
-        </Button>
-      </Box>
-      <Grid container spacing={4} columns={{ xs: 3, sm: 8, md: 12, lg: 12 }}>
-        {sortedProducts.map((product) => (
-          <Grid item key={product.id} size={{ xs: 2, sm: 4, md: 4, lg: 3 }}>
-            <Box
-              onClick={() => console.log(product)}
-              sx={{ cursor: "pointer" }}
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box padding={6}>
+          <Typography variant="h3">Product List</Typography>
+          <Box mb={3} display="flex" gap={2}>
+            <Select
+              value={sortOrder}
+              onChange={(e) => setSortOrder(e.target.value)}
+              displayEmpty
             >
-              <Card>
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={product.image}
-                  alt={product.title}
-                />
-                <CardContent>
-                  <Box
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                  >
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                      {product.title}
-                    </Typography>
-                    <Box sx={{ display: "flex", gap: "5px" }}>
-                      <IconButton
-                        size="small"
-                        color="primary"
-                        onClick={() => navigate(`/edit-product/${product.id}`)}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                      <IconButton
-                        size="small"
-                        color="error"
-                        onClick={() => handleDelete(product.id)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </Box>
-                  </Box>
+              <MenuItem value="asc">Price: Low to High</MenuItem>
+              <MenuItem value="desc">Price: High to Low</MenuItem>
+            </Select>
 
-                  <Tooltip title={product.description}>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{
-                        overflow: "hidden",
-                        display: "-webkit-box",
-                        WebkitLineClamp: 2,
-                        WebkitBoxOrient: "vertical",
-                      }}
-                    >
-                      {product.description}
-                    </Typography>
-                  </Tooltip>
-                  <Typography color="text.secondary">
-                    ${product.price}
-                  </Typography>
-                  <Typography variant="body2">{product.category}</Typography>
-                  <Typography variant="body2">
-                    ⭐ Rating: {product?.rating?.rate ?? "N/A"} (
-                    {product?.rating?.count ?? 0} reviews)
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+            <Select
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+              displayEmpty
+            >
+              <MenuItem value="">All Categories</MenuItem>
+              {categoryList.map((cat) => (
+                <MenuItem key={cat} value={cat}>
+                  {cat}
+                </MenuItem>
+              ))}
+            </Select>
+            <Select
+              value={sortTitle}
+              onChange={(e) => setSortTiltle(e.target.value)}
+              displayEmpty
+            >
+              <MenuItem value="asc">Title: A to Z</MenuItem>
+              <MenuItem value="desc">Title: Z to A</MenuItem>
+            </Select>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/new-product")}
+            >
+              Add Product
+            </Button>
+          </Box>
+          {sortedProducts.length ? (
+            <Grid
+              container
+              spacing={4}
+              columns={{ xs: 3, sm: 8, md: 12, lg: 12 }}
+            >
+              {sortedProducts.map((product) => (
+                <Grid
+                  item
+                  key={product.id}
+                  size={{ xs: 2, sm: 4, md: 4, lg: 3 }}
+                >
+                  <Box
+                    onClick={() => console.log(product)}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <Card>
+                      <CardMedia
+                        component="img"
+                        height="200"
+                        image={product.image}
+                        alt={product.title}
+                      />
+                      <CardContent>
+                        <Box
+                          display="flex"
+                          justifyContent="space-between"
+                          alignItems="center"
+                        >
+                          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                            {product.title}
+                          </Typography>
+                          <Box sx={{ display: "flex", gap: "5px" }}>
+                            <IconButton
+                              size="small"
+                              color="primary"
+                              onClick={() =>
+                                navigate(`/edit-product/${product.id}`)
+                              }
+                            >
+                              <EditIcon />
+                            </IconButton>
+                            <IconButton
+                              size="small"
+                              color="error"
+                              onClick={() => handleDelete(product)}
+                            >
+                              <DeleteIcon />
+                            </IconButton>
+                          </Box>
+                        </Box>
+
+                        <Tooltip title={product.description}>
+                          <Typography
+                            variant="body2"
+                            color="text.secondary"
+                            sx={{
+                              overflow: "hidden",
+                              display: "-webkit-box",
+                              WebkitLineClamp: 2,
+                              WebkitBoxOrient: "vertical",
+                            }}
+                          >
+                            {product.description}
+                          </Typography>
+                        </Tooltip>
+                        <Typography color="text.secondary">
+                          ${product.price}
+                        </Typography>
+                        <Typography variant="body2">
+                          {product.category}
+                        </Typography>
+                        <Typography variant="body2">
+                          ⭐ Rating: {product?.rating?.rate ?? "N/A"} (
+                          {product?.rating?.count ?? 0} reviews)
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <Typography variant="h6">No Product found</Typography>
+          )}
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              <Typography>
+                Are you sure you want to delete{" "}
+                <strong>{productData?.title || "this product"}</strong>?
+              </Typography>
+            </DialogTitle>
+            <DialogContent></DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={confirmDeleteProduct} autoFocus>
+                Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Box>
+      )}
+    </>
   );
 };
 
--- src/components/Product/CreateProduct.jsx
+++ src/components/Product/CreateProduct.jsx
@@ -9,6 +9,7 @@ import {
   FormHelperText,
   MenuItem,
   Select,
+  CircularProgress,
 } from "@mui/material";
 import { useForm, Controller } from "react-hook-form";
 import { useSelector } from "react-redux";
@@ -20,7 +21,7 @@ import {
 } from "../../services/api";
 import { updateCategoty } from "../../redux/actions/categoryAction";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ImageDropzone from "../ImageDropzone/ImageDropzone";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
@@ -29,6 +30,7 @@ import { useParams } from "react-router-dom";
 const CreateProduct = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
   const {
@@ -56,6 +58,8 @@ const CreateProduct = () => {
   }, []);
 
   const getSingalProductData = async () => {
+    setLoading(true);
+
     try {
       const response = await getSingalProductApi(id);
       console.log("responseresponseresponse", response);
@@ -64,8 +68,10 @@ const CreateProduct = () => {
       setValue("price", response.data?.price);
       setValue("image", response.data?.image);
       setValue("description", response.data?.description);
+      setLoading(false);
     } catch (error) {
       console.log("error :>> ", error);
+      setLoading(false);
     }
   };
 
@@ -108,126 +114,149 @@ const CreateProduct = () => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <Box mt={5} sx={{ paddingBottom: "20px" }}>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <Typography variant="h4" gutterBottom>
-            {id ? "Edit Product" : "Create Product"}
-          </Typography>
-        </Box>
+    <>
+      {loading ? (
         <Box
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-          sx={{
-            width: "600px",
-            height: "auto",
-            display: "flex",
-            flexDirection: "column",
-            gap: "20px",
-          }}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+          width="100%"
         >
-          <TextField
-            label="Title"
-            variant="outlined"
-            fullWidth
-            {...register("title", { required: "Title is required" })}
-            error={!!errors.title}
-            helperText={errors.title?.message}
-          />
-
-          <TextField
-            label="Price"
-            variant="outlined"
-            fullWidth
-            type="number"
-            inputProps={{ step: "0.01", min: "0" }}
-            sx={{
-              "& input[type=number]": {
-                MozAppearance: "textfield",
-              },
-              "& input[type=number]::-webkit-outer-spin-button": {
-                WebkitAppearance: "none",
-                margin: 0,
-              },
-              "& input[type=number]::-webkit-inner-spin-button": {
-                WebkitAppearance: "none",
-                margin: 0,
-              },
-            }}
-            {...register("price", {
-              required: "Price is required",
-              min: {
-                value: 0.01,
-                message: "Price must be greater than 0",
-              },
-              valueAsNumber: true,
-            })}
-            error={!!errors.price}
-            helperText={errors.price?.message}
-          />
-          <TextField
-            label="Description"
-            variant="outlined"
-            fullWidth
-            multiline
-            rows={4}
-            {...register("description", {
-              required: "Description is required",
-              minLength: {
-                value: 10,
-                message: "Description must be at least 10 characters",
-              },
-            })}
-            error={!!errors.description}
-            helperText={errors.description?.message}
-          />
-          <FormControl
-            fullWidth
-            variant="outlined"
-            error={!!errors.category}
-            margin="normal"
-          >
-            <InputLabel id="category-label">Category</InputLabel>
-            <Controller
-              name="category"
-              control={control}
-              rules={{ required: "Category is required" }}
-              defaultValue=""
-              render={({ field }) => (
-                <Select labelId="category-label" label="Category" {...field}>
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  {categoryList.map((cat) => (
-                    <MenuItem key={cat} value={cat}>
-                      {cat}
-                    </MenuItem>
-                  ))}
-                </Select>
-              )}
-            />
-            <FormHelperText>{errors.category?.message}</FormHelperText>
-          </FormControl>
-
-          <Controller
-            name="image"
-            control={control}
-            rules={{ required: "Image is required" }}
-            render={({ field, fieldState: { error } }) => (
-              <ImageDropzone
-                value={field.value}
-                onChange={field.onChange}
-                error={error}
-              />
-            )}
-          />
-
-          <Button type="submit" fullWidth variant="contained" color="primary">
-            {id ? "Save" : "Submit"}
-          </Button>
+          <CircularProgress />
         </Box>
-      </Box>
-    </Container>
+      ) : (
+        <Container maxWidth="sm">
+          <Box mt={5} sx={{ paddingBottom: "20px" }}>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Typography variant="h4" gutterBottom>
+                {id ? "Edit Product" : "Create Product"}
+              </Typography>
+            </Box>
+            <Box
+              component="form"
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{
+                width: "600px",
+                height: "auto",
+                display: "flex",
+                flexDirection: "column",
+                gap: "20px",
+              }}
+            >
+              <TextField
+                label="Title"
+                variant="outlined"
+                fullWidth
+                {...register("title", { required: "Title is required" })}
+                error={!!errors.title}
+                helperText={errors.title?.message}
+              />
+
+              <TextField
+                label="Price"
+                variant="outlined"
+                fullWidth
+                type="number"
+                inputProps={{ step: "0.01", min: "0" }}
+                sx={{
+                  "& input[type=number]": {
+                    MozAppearance: "textfield",
+                  },
+                  "& input[type=number]::-webkit-outer-spin-button": {
+                    WebkitAppearance: "none",
+                    margin: 0,
+                  },
+                  "& input[type=number]::-webkit-inner-spin-button": {
+                    WebkitAppearance: "none",
+                    margin: 0,
+                  },
+                }}
+                {...register("price", {
+                  required: "Price is required",
+                  min: {
+                    value: 0.01,
+                    message: "Price must be greater than 0",
+                  },
+                  valueAsNumber: true,
+                })}
+                error={!!errors.price}
+                helperText={errors.price?.message}
+              />
+              <TextField
+                label="Description"
+                variant="outlined"
+                fullWidth
+                multiline
+                rows={4}
+                {...register("description", {
+                  required: "Description is required",
+                  minLength: {
+                    value: 10,
+                    message: "Description must be at least 10 characters",
+                  },
+                })}
+                error={!!errors.description}
+                helperText={errors.description?.message}
+              />
+              <FormControl
+                fullWidth
+                variant="outlined"
+                error={!!errors.category}
+                margin="normal"
+              >
+                <InputLabel id="category-label">Category</InputLabel>
+                <Controller
+                  name="category"
+                  control={control}
+                  rules={{ required: "Category is required" }}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Select
+                      labelId="category-label"
+                      label="Category"
+                      {...field}
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      {categoryList.map((cat) => (
+                        <MenuItem key={cat} value={cat}>
+                          {cat}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                />
+                <FormHelperText>{errors.category?.message}</FormHelperText>
+              </FormControl>
+
+              <Controller
+                name="image"
+                control={control}
+                rules={{ required: "Image is required" }}
+                render={({ field, fieldState: { error } }) => (
+                  <ImageDropzone
+                    value={field.value}
+                    onChange={field.onChange}
+                    error={error}
+                  />
+                )}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+              >
+                {id ? "Save" : "Submit"}
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      )}
+    </>
   );
 };
 
